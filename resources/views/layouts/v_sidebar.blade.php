16aa7f5fa92d35bc3cef4fd21948e221cee4ee6a
dandimointi1609 <dandirivaldimointi09@gmail.com>
2022-12-16 06:29:07 +0800

    first commit

---
+++ resources/views/layouts/v_sidebar.blade.php
@@ -0,0 +1,87 @@
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-label>
+              <span class="menu-title">Master Data</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          {{-- <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">UI Elements</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+              </ul>
+            </div>
+          </li> --}}
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Data Kriteria</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Data Sub Kriteria</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="mdi mdi-account-multiple menu-icon"></i>
+              <span class="menu-title">Calon Penrima Bantuan</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="mdi mdi-pencil-box-outline menu-icon"></i>
+              <span class="menu-title">Data Penilaian</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="mdi mdi-counter menu-icon"></i>
+              <span class="menu-title">Data Perhitungan</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="mdi mdi-chart-bar menu-icon"></i>
+              <span class="menu-title">Laporan Hasil Akhir</span>
+            </a>
+          </li>
+
+          <li class="nav-label>
+            <span class="menu-title">Master User</span>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-account-circle menu-icon"></i>
+            <span class="menu-title">Data User</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-account-multiple-outline menu-icon"></i>
+            <span class="menu-title">Data Profile</span>
+          </a>
+        </li>
+          
+      </nav>
---
+++ resources/views/v_home.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.v_master')
+
+@section('content')
+<div class="main-panel">
+<div class="content-wrapper">
+  <div class="row">
+    <div class="col-md-12 grid-margin">
+      <div class="row">
+        <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+          <h3 class="font-weight-bold">Welcome Aamir</h3>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card card-tale">
+        <div class="card-body">
+          <p class="mb-4">Data Kriteria</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card card-dark-blue">
+        <div class="card-body">
+          <p class="mb-4">Data Sub Kriteria</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card card-light-blue">
+        <div class="card-body">
+          <p class="mb-4">Calon Penerima Bantuan</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card card-tale">
+        <div class="card-body">
+          <p class="mb-4">Data Penilaian</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card card-light-danger">
+        <div class="card-body">
+          <p class="mb-4">Data Perhitungan</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 stretch-card grid-margin">
+      <div class="card tale-bg">
+        <div class="card-body">
+          <p class="mb-4">Laporan Hasil Akhir</p>
+          <p class="fs-30 mb-2">4006</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+@endsection
